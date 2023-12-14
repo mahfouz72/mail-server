7f31984cff8f07f61408cae53376f7cc532c1a69
mahfouz72 <mohamadmahfouz39@gmail.com>
2023-12-14 15:58:33 +0200

    added frontend directory

---
+++ Frontend/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>{{msg}}</h2>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      msg: 'hello'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
