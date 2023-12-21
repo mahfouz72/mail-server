c3157fa7750411feb8eea99befacb7d6145db0ea
Youssif Khaled <116981986+YoussifKhaled@users.noreply.github.com>
2023-12-21 02:45:46 +0200

    Add Pagination and initial email listing

--- Frontend/App.vue
+++ Frontend/App.vue
@@ -19,7 +19,7 @@
     <NavBar/>
     <div style="display: flex;">
       <SideBar :currentFolder = "currentFolder" @open="open"/>
-      <MainBoard/>
+      <MainBoard :emails = "emails"/>
     </div>
     <footer>
       ©2023 OOP Assingment 4
@@ -43,6 +43,7 @@ export default {
         email: '',
         userLoggedIn: true,
         currentFolder: 'Inbox',
+        emails: ['email1', 'email2', 'email3', 'email4', 'email5', 'email6', 'email7', 'email8', 'email9', 'email10', 'email11', 'email12', 'email13', 'email14', 'email15', 'email16', 'email17', 'email18', 'email19', 'email20', 'email21', 'email22', 'email23', 'email24', 'email25', 'email26', 'email27', 'email28', 'email29', 'email30', 'email31', 'email32', 'email33', 'email34', 'email35', 'email36', 'email37', 'email38', 'email39', 'email40', 'email41', 'email42', 'email43', 'email44', 'email45', 'email46', 'email47', 'email48', 'email49', 'email50', 'email51', 'email52', 'email53', 'email54', 'email55', 'email56', 'email57', 'email58', 'email59', 'email60', 'email61', 'email62', 'email63', 'email64', 'email65', 'email66', 'email67', 'email68', 'email69', 'email70', 'email71', 'email72', 'email73', 'email74', 'email75', 'email76', 'email77', 'email78', 'email79', 'email80', 'email81', 'email82', 'email83', 'email84', 'email85', 'email86', 'email87', 'email88', 'email89', 'email90', 'email91', 'email92', 'email93', 'email94', 'email95', 'email96', 'email97', 'email98', 'email99', 'email100', 'email101', 'email102', 'email103', 'email104', 'email105', 'email106', 'email107', 'email108', 'email109', 'email110', 'email111', 'email112', 'email113', 'email114', 'email115', 'email116', 'email117', 'email118', 'email119', 'email120', 'email121', 'email122', 'email123', 'email124', 'email125', 'email126', 'email127', 'email128', 'email129', 'email130', 'email131', 'email132', 'email133', 'email134', 'email135', 'email136', 'email137', 'email138', 'email139', 'email140', 'email141', 'email142', 'email143', 'email144', 'email145', 'email146', 'email147', 'email148', 'email149', 'email150', 'email151', 'email152', 'email153', 'email154', 'email155', 'email156', 'email157', 'email158', 'email159', 'email160', 'email161', 'email162', 'email163', 'email164', 'email165', 'email166', 'email167', 'email168', 'email169', 'email170', 'email171', 'email172', 'email173', 'email174', 'email175', 'email176', 'email177', 'email178', 'email179', 'email180', 'email181', 'email182', 'email183', 'email184', 'email185', 'email186', 'email187', 'email188', 'email189', 'email190', 'email191', 'email192', 'email193', 'email194', 'email195', 'email196', 'email197', 'email198', 'email199', 'email200']
       }
     },
     methods: {
--- Frontend/components/MainBoard.vue
+++ Frontend/components/MainBoard.vue
@@ -1,16 +1,64 @@
 <template>
     <div class="mainboard">
-        <!-- Your navbar content here -->
-        <ComposeWindow></ComposeWindow>
+        <!-- <ComposeWindow></ComposeWindow> -->
+        <div class="email-list">
+            <div class="email" v-for="(email, index) in paginatedEmails" :key="index">
+                {{ email }}
+            </div>
+        </div>
+        <div class="pagination">
+            <button class="pi pi-angle-left paginationBtn" @click="previousPage" :disabled="currentPage === 1"></button>
+            <div class="page-numbers">
+                <button v-for="pageNumber in displayedPageNumbers" :key="pageNumber" class="nmbrBtn" :class="{ active: pageNumber === currentPage }" @click="goToPage(pageNumber)">{{ pageNumber }}</button>
+            </div>
+            <button class="pi pi-angle-right paginationBtn" @click="nextPage" :disabled="currentPage === totalPages"></button>
+        </div>
     </div>
 </template>
 
 <script>
-import ComposeWindow from './ComposeWindow.vue';
+// import ComposeWindow from './ComposeWindow.vue';
 export default {
     name: 'MainBoard',
-    components:{ComposeWindow}
-    // Your component's logic here
+    // components:{ComposeWindow}
+    props: ['emails'],
+    data() {
+        return {
+            currentPage: 1,
+            emailsPerPage: 10,
+        };
+    },
+    computed: {
+        paginatedEmails() {
+            const startIndex = (this.currentPage - 1) * this.emailsPerPage;
+            const endIndex = startIndex + this.emailsPerPage;
+            return this.emails.slice(startIndex, endIndex);
+        },
+        totalPages() {
+            return Math.ceil(this.emails.length / this.emailsPerPage);
+        },
+        displayedPageNumbers() {
+            const range = 10;
+            const startPage = Math.max(1, this.currentPage - Math.floor(range / 2));
+            const endPage = Math.min(this.totalPages, startPage + range - 1);
+            return Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i);
+        },
+    },
+    methods: {
+        previousPage() {
+            if (this.currentPage > 1) {
+                this.currentPage--;
+            }
+        },
+        nextPage() {
+            if (this.currentPage < this.totalPages) {
+                this.currentPage++;
+            }
+        },
+        goToPage(pageNumber) {
+            this.currentPage = pageNumber;
+        },
+    },
 }
 </script>
 
@@ -21,5 +69,57 @@ export default {
     width: 80vw;
     margin: 0;
     border-bottom: 0.1vw solid black;
+}
+.email-list {
+    height: 90%;
+    width: 100%;
+    justify-content: flex-start;
+    align-items: center;
+}
+.email {
+    background-color: rgba(127, 129, 132, 0.821);
+    height: 6.5vh;
+    width: 95%;
+    margin: 1vh;
+    border-radius: 10px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding-left: 1vw;
+    font-size: 2vh;
+    color: black;
+}
+.email:hover {
+    background-color: rgba(127, 129, 132, 0.5);
+    color: white;
+    cursor: pointer;
+}
+.pagination {
+    margin-top: 2vh;
+}
+.paginationBtn {
+    background-color: transparent;
+    border: none;
+    color: white;
+    font-size: 3vh;;
+    cursor: pointer;
+}
+.paginationBtn:disabled{
+    color: grey;
+}
+.page-numbers {
+    display: inline-flex;
+    font-size: 1vh;
+    transform: translate(0, -0.5vh);
+}
+.nmbrBtn {
+    background-color: transparent;
+    border: none;
+    color: white;
+    font-size: 2vh;
+    cursor: pointer;
+}
+.nmbrBtn.active {
+    color: grey;
 }
 </style>
