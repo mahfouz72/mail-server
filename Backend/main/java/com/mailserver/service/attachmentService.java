75e2664c53f1d80add5914b5ba98b405ed234d08
esmailMahmouds <120949930+esmailMahmouds@users.noreply.github.com>
2023-12-25 16:11:19 +0200

    merge attachment

--- Backend/main/java/com/mailserver/service/attachmentService.java
+++ Backend/main/java/com/mailserver/service/attachmentService.java
@@ -11,33 +11,39 @@ import java.util.Map;
 import java.util.UUID;
 
 @Service
-public class attachmentService {
-    private final Map<String, Attachment> db=new HashMap<>(){};
+public class AttachmentService {
+    private final Map<String, Attachment> db = new HashMap<>();
 
-    public Collection<Attachment> get(){return db.values();}
-    public Attachment get(String id){
+    public Collection<Attachment> get() {
+        return db.values();
+    }
+
+    public Attachment get(String id) {
         return db.get(id);
     }
-    public Attachment remove(String id){
-        return  db.remove(id);
+
+    public Attachment remove(String id) {
+        return db.remove(id);
     }
-    public Attachment upload(String fileName, String contentType, byte[] data){
-        String id=UUID.randomUUID().toString();
-        Attachment attachment =new Attachment(id,fileName,contentType,data);
-        db.put(attachment.getId(),attachment);
+
+    public Attachment upload(String fileName, String contentType, byte[] data) {
+        String id = UUID.randomUUID().toString();
+        Attachment attachment = new Attachment(id, fileName, contentType, data);
+        db.put(attachment.getId(), attachment);
         return attachment;
     }
-    public ResponseEntity<byte[]>download( String id){
+
+    public ResponseEntity<byte[]> download(String id) {
         Attachment attachment = db.get(id);
-        if(attachment == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        byte[] data=attachment.getData();
-        HttpHeaders headers=new HttpHeaders();
+        if (attachment == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        byte[] data = attachment.getData();
+        HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.valueOf(attachment.getContentType()));
-        ContentDisposition build =ContentDisposition
+        ContentDisposition build = ContentDisposition
                 .builder("attachment")
                 .filename(attachment.getFileName())
                 .build();
         headers.setContentDisposition(build);
-        return new ResponseEntity<>(data,headers,HttpStatus.OK);
+        return new ResponseEntity<>(data, headers, HttpStatus.OK);
     }
 }
