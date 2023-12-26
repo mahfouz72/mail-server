83ae572bf6aa6f75236877d3495d202f4b85d24c
esmailMahmouds <120949930+esmailMahmouds@users.noreply.github.com>
2023-12-26 02:25:08 +0200

    edit attachment in draft

--- Backend/main/java/com/mailserver/service/DraftService.java
+++ Backend/main/java/com/mailserver/service/DraftService.java
@@ -1,19 +1,29 @@
 package com.mailserver.service;
 
+import com.mailserver.model.Attachment;
 import com.mailserver.model.mail.Mail;
 import com.mailserver.model.mail.MailBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class DraftService {
     private UserService userService = UserService.getInstance();
-
-    public Mail addDraft(String email, Mail mail) {
+    @Autowired
+    private AttachmentService attachmentService;
+    public Mail addDraft(String email, Mail mail, ArrayList<String> attachmentIds) {
         mail.setId(Long.toString(System.currentTimeMillis()));
         mail.setDateTime(LocalDateTime.now().withNano(0));
+        ArrayList<Attachment> attachments=new ArrayList<Attachment>();
+        for (String attachmentId : attachmentIds) {
+            attachments.add(attachmentService.get(attachmentId));
+            attachmentService.remove(attachmentId);
+        }
+        mail.setAttachments(attachments);
         userService.getUserByEmail(email).getDrafts().add(mail);
         return mail;
     }
