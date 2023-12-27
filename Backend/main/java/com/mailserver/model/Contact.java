739c14bc97afa8e1fb0e4e7e5860a69ab401b925
Youssif Khaled <116981986+YoussifKhaled@users.noreply.github.com>
2023-12-27 18:18:55 +0200

    Add files via upload

--- Backend/main/java/com/mailserver/model/Contact.java
+++ Backend/main/java/com/mailserver/model/Contact.java
@@ -4,40 +4,52 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Component
-public class Contact implements Comparable<Contact>{
+public class Contact implements Comparable<Contact> {
     private String name;
     private List<String> emailAddresses;
-    public Contact(){}
-    public Contact(String name,String emailAddress){
+
+    public Contact() {
+    }
+
+    public Contact(String name, String emailAddress) {
         this.name = name;
         this.emailAddresses = new ArrayList<>();
         emailAddresses.add(emailAddress);
     }
-    public Contact(String name,List<String> emailAddresses){
-        this.name=name;
-        this.emailAddresses=emailAddresses;
-    }
-    public void editName(String name){
 
-        this.name=name;
+    public Contact(String name, List<String> emailAddresses) {
+        this.name = name;
+        this.emailAddresses = emailAddresses;
     }
-    public void addMail(String emailAddress){
 
-        emailAddresses.add(emailAddress);
+    public String getName() {
+        return this.name;
     }
-    public void deleteMail(int emailIndex){
-        emailAddresses.remove(emailIndex);
+
+    public List<String> getEmailAddresses() {
+        return this.emailAddresses;
     }
-    public String getName(){return this.name;}
-    public List<String>getEmailAddresses(){return this.emailAddresses;}
 
     @Override
-    public int compareTo(Contact other){
+    public int compareTo(Contact other) {
         return this.name.compareTo(other.name);
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setEmailAddress(int email_index, String newEmailAddress) {
+        emailAddresses.set(email_index, newEmailAddress);
+    }
+
+    public void addEmailAddress(String newEmailAddress) {
+        emailAddresses.add(newEmailAddress);
+    }
+
+    public void removeEmailAddress(String removedEmailAddress) {
+        emailAddresses.remove(removedEmailAddress);
     }
 }
